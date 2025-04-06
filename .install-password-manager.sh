c34995bef4ee8cbc5de1001514955e749ace0e6f
poponta <34686350+poponta1218@users.noreply.github.com>
2025-04-07 06:16:54 +0900

    feat(mise,bitwarden): ✨ add installation script for mise and Bitwarden CLI hooks
    link #8

---
+++ .install-password-manager.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# vim: set ft=sh:
+
+# Install mise
+if [[ ! -e "${HOME}/.local/bin/mise" ]]; then
+  echo "Installing mise..."
+  curl https://mise.run | MISE_INSTALL_PATH="${HOME}/.local/bin/mise" sh
+  echo "mise installed successfully."
+fi
+
+if [[ "$(type -P bw)" == "" ]]; then
+  echo "Installing Bitwarden CLI..."
+  "${HOME}"/.local/bin/mise use --global bitwarden
+else
+  echo "Bitwarden CLI is already installed."
+fi
