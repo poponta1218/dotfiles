43400a2f0668bc697cc43f20050875137231cca1
poponta <34686350+poponta1218@users.noreply.github.com>
2025-04-09 04:31:07 +0900

    refactor(mise): ♻️ move mise installation to the template script for clarity

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 # vim: set ft=sh:
 
-# Install mise
-if [[ ! -e "${HOME}/.local/bin/mise" ]]; then
-  echo "Installing mise..."
-  curl https://mise.run | MISE_INSTALL_PATH="${HOME}/.local/bin/mise" sh
-  echo "mise installed successfully."
-fi
-
 if [[ "$(type -P bw)" == "" ]]; then
   case "$(uname -s)" in
   Linux)
